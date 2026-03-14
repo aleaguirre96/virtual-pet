01063df1ce2e93ad6c4858f796068da17ad16ae8
aleaguirre96 <jta.guirre9@gmail.com>
2026-03-14 06:00:46 -0600

    Added mood system and sprite switching based on happiness

--- src/VirtualPet.ts
+++ src/VirtualPet.ts
@@ -1,7 +1,31 @@
-export class VirtualPet {
-    happiness: number = 0
+export const PetMood = {
+  Sad: "sad",
+  Neutral: "neutral",
+  Happy: "happy",
+} as const;
 
-    addHappiness(amount: number) {
+export type PetMood = (typeof PetMood)[keyof typeof PetMood];
+
+export class VirtualPet {
+    happiness: number = 50;
+
+
+    increaseHappiness(amount: number) {
         this.happiness += amount
+    }
+    
+    decreaseHappiness(amount: number) {
+        
+        this.happiness -= amount
+    }
+
+    getMood(): PetMood {
+        if (this.happiness < 20) {
+            return PetMood.Sad
+        } else if (this.happiness > 80) {
+            return PetMood.Happy
+        } else {
+            return PetMood.Neutral
+        }
     }
 }
--- src/main.ts
+++ src/main.ts
@@ -2,18 +2,44 @@ import { VirtualPet } from "./VirtualPet";
 
 const pet = new VirtualPet()
 
+const petSprites = {
+  sad: "/assets/pet/pet-sad.png",
+  neutral: "/assets/pet/pet-neutral.png",
+  happy: "/assets/pet/pet-happy.png"
+};
+
 const app = document.querySelector<HTMLDivElement>('#app')!
 
 app.innerHTML = `
-<h1>Virtual Pet</h1>
-<button id="pet">Pet</button>
-<p>Happiness: <span id="happiness">0</span></p>
+  <div>
+    <h1>Virtual Pet</h1>
+    <img id="pet" src="/assets/pet/pet-neutral.png" width="200" />
+    <br />
+    <button id="pet-button-increase">Increase</button>
+    <p>Happiness: <span id="happiness">0</span></p>
+  </div>
 `
 
-const button = document.querySelector<HTMLButtonElement>('#pet')!
+const petImage = document.querySelector<HTMLImageElement>("#pet")!;
+const buttonIncrease = document.querySelector<HTMLButtonElement>('#pet-button-increase')!
 const happinessText = document.querySelector<HTMLSpanElement>('#happiness')!
 
-button.addEventListener("click", () => {
-  pet.addHappiness(1)
-  happinessText.textContent = pet.happiness.toString()
+buttonIncrease.addEventListener("click", () => {
+  pet.increaseHappiness(5)
+  
+  updatePetVisual()
 })
+
+function updatePetVisual() {
+  const mood = pet.getMood();
+  petImage.src = petSprites[mood];
+  happinessText.textContent = pet.happiness.toString()
+  console.log("inside updatePetVisual" + petSprites[mood] )
+}
+
+updatePetVisual();
+
+setInterval(() => {
+  pet.decreaseHappiness(5)
+  updatePetVisual()
+}, 30000)
